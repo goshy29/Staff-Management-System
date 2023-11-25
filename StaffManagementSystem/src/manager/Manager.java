27cea03d7f8f636171dd90f36065390bd3c78598
goshy <georgi.dobromirov@gmail.com>
2023-11-25 09:15:53 +0200

    add changes in Manager.java file

--- StaffManagementSystem/src/manager/Manager.java
+++ StaffManagementSystem/src/manager/Manager.java
@@ -20,74 +20,77 @@ public class Manager {
     }
 
     public int execute(int menuIndex) {
-        if (menuIndex == 1) {
-            HashMap<Integer, Employee> map = service.getAllEmployees();
-            int id = ValidInt.validateIntegerInput("Enter id: ");
-            if (map.containsKey(id)) {
-                System.out.printf("Id %s already exist!", id);
-            } else {
-                String name = ValidString.validateStringInput("Enter name: ");
+        HashMap<Integer, Employee> map;
+        switch (menuIndex) {
+            case 1:
+                map = service.getAllEmployees();
+                int id = ValidInt.validateIntegerInput("Enter id: ");
+                if (map.containsKey(id)) {
+                    System.out.printf("Id %s already exist!", id);
+                } else {
+                    String name = ValidString.validateStringInput("Enter name: ");
 
-                LocalDateTime now = LocalDateTime.now();
-                String startDate = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+                    LocalDateTime now = LocalDateTime.now();
+                    String startDate = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
-                String department = ValidString.validateStringInput("Enter department: ");
+                    String department = ValidString.validateStringInput("Enter department: ");
+                    String role = ValidString.validateStringInput("Enter role: ");
+                    double salary = ValidDouble.validateDoubleInput("Enter salary: ");
+                    Employee employee = new Employee(id, name, startDate,
+                            Optional.empty(), department, role, salary);
+                    service.addEmployee(employee);
+                }
+                menuIndex = backMenu();
+                break;
+            case 2:
+                map = service.getAllEmployees();
+                int keyToUpdate = ValidInt.validateIntegerInput("Enter id to update: ");
+                if (map.containsKey(keyToUpdate)) {
+                    String name = ValidString.validateStringInput("Enter name: ");
+
+                    LocalDateTime now = LocalDateTime.now();
+                    String startDate = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+                    String department = ValidString.validateStringInput("Enter department: ");
+                    String role = ValidString.validateStringInput("Enter role: ");
+                    double salary = ValidDouble.validateDoubleInput("Enter salary: ");
+                    Employee updatedEmployee = new Employee(keyToUpdate, name, startDate,
+                            Optional.empty(), department, role, salary);
+                    service.editEmployee(keyToUpdate, updatedEmployee);
+                } else {
+                    System.out.println("Invalid key. Record not found.");
+                }
+                menuIndex = backMenu();
+                break;
+            case 3:
+                map = service.getAllEmployees();
+                for (HashMap.Entry<Integer, Employee> entry : map.entrySet()) {
+                    System.out.printf("%d. name: %s, department: %s, position: %s, salary: %.2f%n", entry.getValue().getId(),
+                            entry.getValue().getName(), entry.getValue().getDepartment(), entry.getValue().getRole(), entry.getValue().getSalary());
+                }
+                menuIndex = backMenu();
+                break;
+            case 4:
                 String role = ValidString.validateStringInput("Enter role: ");
-                double salary = ValidDouble.validateDoubleInput("Enter salary: ");
-                Employee employee = new Employee(id, name, startDate,
-                        Optional.empty(), department, role, salary);
-                service.addEmployee(employee);
-            }
-            menuIndex = backMenu();
-        }
-        if (menuIndex == 2) {
-            HashMap<Integer, Employee> map = service.getAllEmployees();
-            int keyToUpdate = ValidInt.validateIntegerInput("Enter id to update: ");
-            if (map.containsKey(keyToUpdate)) {
-                String name = ValidString.validateStringInput("Enter name: ");
+                service.getEmployeesByRole(role);
+                menuIndex = backMenu();
+                break;
+            case 5:
+                int employeeId = ValidInt.validateIntegerInput("Enter id: ");
+                service.getEmployeesById(employeeId);
+                menuIndex = backMenu();
+                break;
 
-                LocalDateTime now = LocalDateTime.now();
-                String startDate = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-
+            case 7:
+                String name = ValidString.validateStringInput("Enter full name: ");
+                service.getEmployeesByName(name);
+                menuIndex = backMenu();
+                break;
+            case 8:
                 String department = ValidString.validateStringInput("Enter department: ");
-                String role = ValidString.validateStringInput("Enter role: ");
-                double salary = ValidDouble.validateDoubleInput("Enter salary: ");
-                Employee updatedEmployee = new Employee(keyToUpdate, name, startDate,
-                        Optional.empty(), department, role, salary);
-                service.editEmployee(keyToUpdate, updatedEmployee);
-            } else {
-                System.out.println("Invalid key. Record not found.");
-            }
-            menuIndex = backMenu();
-        }
-        if (menuIndex == 3) {
-            HashMap<Integer, Employee> map = service.getAllEmployees();
-            for (HashMap.Entry<Integer, Employee> entry : map.entrySet()) {
-                System.out.printf("%d. name: %s, department: %s, position: %s, salary: %.2f%n", entry.getValue().getId(),
-                        entry.getValue().getName(), entry.getValue().getDepartment(), entry.getValue().getRole(), entry.getValue().getSalary());
-            }
-            menuIndex = backMenu();
-        }
-        if (menuIndex == 4) {
-            String role = ValidString.validateStringInput("Enter role: ");
-            service.getEmployeesByRole(role);
-            menuIndex = backMenu();
-        }
-        if (menuIndex == 5) {
-            int id = ValidInt.validateIntegerInput("Enter id: ");
-            service.getEmployeesById(id);
-            menuIndex = backMenu();
-        }
-
-        if (menuIndex == 7) {
-            String name = ValidString.validateStringInput("Enter full name: ");
-            service.getEmployeesByName(name);
-            menuIndex = backMenu();
-        }
-        if (menuIndex == 8) {
-            String department = ValidString.validateStringInput("Enter department: ");
-            service.getEmployeesByDepartment(department);
-            menuIndex = backMenu();
+                service.getEmployeesByDepartment(department);
+                menuIndex = backMenu();
+                break;
         }
         return menuIndex;
     }
